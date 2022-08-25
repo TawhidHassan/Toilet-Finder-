78586131d1dcc359e02b956d3e8b19e4e52bca58
sifat hassan <sifat007.sh7@gmail.com>
2022-08-25 11:45:18 +0600

    install

---
+++ lib/Constants/Colors/app_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFEF5777);
+const kPrimaryColorShade = Color(0xFFE0EEFF);
+const kPrimaryLightColor = Color(0xFFF2F7FF);
+const whiteBackground = Color(0xFFffffff);
+const BackgroundColorx = Color(0xFFF6F8FF);
+const blueShade = Color(0xFFADD0FF);
+const black = Color(0xFF1F1F1F);
+const textFieldBorder = Color(0xFF94A3B8);
+
+const Color myColor = Color(0xffEF5777 );
+
+const MaterialColor kPrimaryColorx = const MaterialColor(
+  0xFFEF5777,
+  const <int, Color>{
+    50: const Color(0xFFEF5777),
+    100: const Color(0xFFEF5777),
+    200: const Color(0xFFEF5777),
+    300: const Color(0xFFEF5777),
+    400: const Color(0xFFEF5777),
+    500: const Color(0xFFEF5777),
+    600: const Color(0xFFEF5777),
+    700: const Color(0xFFEF5777),
+    800: const Color(0xFFEF5777),
+    900: const Color(0xFFEF5777),
+  },
+);
---
+++ lib/Constants/Strings/app_strings.dart
@@ -0,0 +1,17 @@
+///firebase api
+const String BASE_URL_FIREBASE = 'https://fcm.googleapis.com/fcm/send';
+const String KEY_SERVER = 'AAAAU1d6Qbg:APA91bF9w1-80B0ef4YoOxMUOZfP-b8IR1NMoZwSUPwVWP2ArhnSrIHlMGj-C20ZSep2TzgRUUzE8dJphsrCNhtUDKnNYUl213El1sO_Y0jeKVwHy9NbO-cyhgqqshYv5AbBdLSTjOlI';
+const String SENDER_ID = '357949915576';
+
+
+//base url of api
+//staging
+const BASE_URL = "https://staging-api.field-x.org/api/v1/";
+const BASE_URL_IMAGE = "https://staging-fieldx.sgp1.digitaloceanspaces.com/";
+
+
+//route name
+const MAIN_PAGE = "/main_page";
+const HOME_PAGE = "/home_page";
+const LOGIN_PAGE = "/login_page";
+const FORGET_PASSWORD_PAGE = "/forget_pass_page";
